52e8409f363db126ad8007c5518f637e6030b787
how2fft <how2fft@proton.me>
2026-06-23 22:43:18 +0800

    Add mobile app as submodule

--- apps/mobile/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import { CapacitorConfig } from '@capacitor/cli'
-
-const config: CapacitorConfig = {
-  appId: 'com.linx.app',
-  appName: 'LinX',
-  webDir: '../web/dist',
-}
-
-export default config
